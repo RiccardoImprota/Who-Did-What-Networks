e8ae8d504a87519a1afbb4d46f62e5f2b478efae
RiccardoImprota <riccardo.imp@gmail.com>
2024-10-11 17:05:09 +0200

    CRAP

--- whodidwhat/resources.py
+++ whodidwhat/resources.py
@@ -6,11 +6,11 @@ import subprocess
 def _valences(language="english"):
 
     if language == "english":
-        from valence.english import _positive, _negative, _ambivalent
+        from whodidwhat.valence.english import _positive, _negative, _ambivalent
 
 
     elif language == "italian":
-        from valence.italian import _positive, _negative, _ambivalent
+        from whodidwhat.valence.italian import _positive, _negative, _ambivalent
 
     return _positive, _negative, _ambivalent
 
