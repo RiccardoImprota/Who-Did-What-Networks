8a3c8d074af924e00356953110e57a5f728b7793
RiccardoImprota <riccardo.imp@gmail.com>
2024-10-11 12:57:39 +0200

    Setup Package

---
+++ WDW-networks/__init__.py
@@ -0,0 +1,3 @@
+from .wdwnetworks import *
+
+__version__ = "0.1"
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+# Get the requirements from the requirements.txt file
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='WhoDidWhat',
+    url='https://github.com/RiccardoImprota/Who-did-What-Networks',
+    author='Riccardo Improta',
+    author_email='riccardo.imp@gmail.com',
+    packages=find_packages(),
+    install_requires=requirements,
+    version='0.1',
+    license='BSD-3-Clause license',
+    description='An example of a python package from pre-existing code',
+        
+)
